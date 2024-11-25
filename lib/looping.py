221cfc74aff9916072cc4f04827df233597d4eec
Bravin Murambi <bravvjr@gmail.com>
2024-11-25 22:40:02 +0300

    Adds assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
+    k = 10
+    while k > 0:
+        print(k)
+        k -= 1
+        if k == 0:
+            print("Happy New Year!")
     pass
-
+print(happy_new_year())
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [i * i for i in int_list]
+pass
 
 def fizzbuzz():
-    # code goes here!
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     pass
+print(fizzbuzz())
